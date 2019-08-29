c5a06fb92b74196d5a9a8586b887541e77fc1897
vrplantsncsu <51127119+vrplantsncsu@users.noreply.github.com>
2019-08-29 15:32:39 -0400

    Fixing seed spinning junk

--- Herbarium 6-3 (Quest) - LWRP/Assets/MapleSpinner.cs
+++ Herbarium 6-3 (Quest) - LWRP/Assets/MapleSpinner.cs
@@ -13,8 +13,6 @@ public class MapleSpinner : MonoBehaviour
     [SerializeField]
     [Range(0f, 1.0f)]
     private float generalDragContribution;
-    //[SerializeField]
-    //private float rightingVariance;
     [SerializeField]
     private float rightingForce;
     [SerializeField]
@@ -23,28 +21,50 @@ public class MapleSpinner : MonoBehaviour
     //private float spinLimit;
     [SerializeField]
     private float maxAngularVelocity;
-    private Vector3 yDiff;
     [SerializeField]
     private float minDistance;
-    // Start is called before the first frame update
+    [SerializeField]
+    private bool doubleSided = true;
+    [SerializeField]
+    private bool immediateRightingEnabled = false;
+
+    private Vector3 targetVector;
+    private Vector3 yDiff;
     void Start()
     {
         body = gameObject.GetComponent<Rigidbody>();
         body.maxAngularVelocity = maxAngularVelocity;
+        targetVector = Vector3.up;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if ((Vector3.up - body.transform.up.normalized).magnitude <= Mathf.Sqrt(2))
+            targetVector = Vector3.up;
+        else
+            targetVector = -Vector3.up;
+        yDiff = targetVector - body.transform.up.normalized;
+        Debug.DrawLine(body.transform.position, body.transform.position + targetVector, Color.green);
+        //Debug.DrawLine(body.transform.position + body.transform.up.normalized, (body.transform.position + body.transform.up.normalized) + targetVector * this.rightingForce * yDiff.magnitude / Mathf.Sqrt(2), Color.red);
+        Debug.DrawLine(body.transform.position + body.transform.up.normalized, (body.transform.position + body.transform.up.normalized) + targetVector * this.rightingForce, Color.red);
+
+        Debug.DrawLine(body.transform.position + body.transform.up.normalized, body.transform.position + targetVector.normalized);
         body.maxAngularVelocity = maxAngularVelocity;
         if (body.velocity.y < yLimit)
         {
-            
-            applyTorque();
-            applyDrag();
-            yDiff = Vector3.up.normalized - body.transform.up.normalized;
+            yDiff = targetVector - body.transform.up.normalized;
+            //yDiff = Vector3.up.normalized - body.transform.up.normalized;
             //yDiff = body.transform.up - Vector3.up;
             applyRightingForce();
+            applyTorque();
+            applyDrag();
+            
             /*
             if ((yDiff).magnitude > rightingVariance)
             {
@@ -53,14 +73,45 @@ public class MapleSpinner : MonoBehaviour
             */
         }
     }
-
     void applyRightingForce()
     {
-        Vector3 prevDiff = yDiff;
-        body.AddTorque(Vector3.right * rightingForce * yDiff.z);
-        body.AddTorque(Vector3.forward * rightingForce * -yDiff.x);
+        //Vector3 prevDiff = yDiff;
+        Vector3 rightingForce;
+        Vector3 forcePos;
+        //rightingForce = targetVector * this.rightingForce * (yDiff.magnitude / Mathf.Sqrt(2) + 1);
+        rightingForce = targetVector * this.rightingForce;
+        forcePos = body.transform.up;
+        /*
+        if (yDiff.magnitude <= Mathf.Sqrt(2) || doubleSided == false)
+        {
+            //rightingForce = (yDiff) * yDiff.magnitude / Mathf.Sqrt(2) * this.rightingForce;
+            rightingForce = Vector3.up * this.rightingForce * yDiff.magnitude/Mathf.Sqrt(2);
+            forcePos = body.transform.up;
+        }
+        else
+        {
+            Vector3 tempDiff = -Vector3.up - body.transform.up.normalized;
+            // rightingForce = (tempDiff) * tempDiff.magnitude/Mathf.Sqrt(2) * this.rightingForce;
+            rightingForce = -Vector3.up * this.rightingForce * tempDiff.magnitude / Mathf.Sqrt(2);
+            forcePos = -body.transform.up;
+        }
+        */
+        body.AddForceAtPosition(rightingForce, body.transform.position + forcePos);
+        //body.AddForceAtPosition()
+        //yDiff = body.transform.InverseTransformDirection(yDiff);
+        /*if(yDiff.magnitude <= Mathf.Sqrt(2))
+        {
+            body.AddTorque(body.transform.right * rightingForce * -yDiff.z);
+            body.AddTorque(body.transform.forward * rightingForce * yDiff.x);
+        }
+        else
+        {
+            body.AddTorque(Vector3.right * rightingForce * yDiff.z);
+            body.AddTorque(Vector3.forward * rightingForce * -yDiff.x);
+        }
+        */
         //body.AddForceAtPosition(Vector3.up * rightingForce, body.transform.position + Vector3.up + Vector3.forward + Vector3.right);
-        yDiff = Vector3.up.normalized - body.transform.up.normalized;
+        //yDiff = Vector3.up.normalized - body.transform.up.normalized;
 
         //if(yDiff.magnitude > prevDiff.magnitude)
         //{
@@ -91,8 +142,18 @@ public class MapleSpinner : MonoBehaviour
 
     void applyTorque()
     {
-        body.AddTorque(Vector3.up * spinTorque * 1 / (yDiff.magnitude + 1) , ForceMode.Acceleration);
+        body.AddTorque(Vector3.up * spinTorque * 1 / (yDiff.magnitude + 1), ForceMode.Acceleration);
+        /*
+        if (yDiff.magnitude <= Mathf.Sqrt(2))
+        {
+            body.AddTorque(Vector3.up * spinTorque * 1 / (yDiff.magnitude + 1), ForceMode.Acceleration);
+        }else
+        {
+            Vector3 tempDiff = -Vector3.up - body.transform.up.normalized;
+            body.AddTorque(Vector3.up * spinTorque * 1 / (tempDiff.magnitude + 1), ForceMode.Acceleration);
+        }
         //body.rotation = Quaternion.Euler(0, body.rotation.eulerAngles.y + spinTorque, 0);
         //body.angularVelocity += new Vector3(0, spinTorque, 0);
+        */
     }
 }
